ab998e8a36ba505e94b180da47183f958f513d62
chadgalusha <chadgalusha83@gmail.com>
2023-07-19 16:31:39 -0500

    added console app to generate jwt for testing. added authorize tag to public chat and messages controllers

---
+++ JWTGenerator/JwtProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTGenerator
+{
+    public static class JwtProcessor
+    {
+        private static readonly SymmetricSecurityKey securityKey = new(Encoding.ASCII.GetBytes("Iyg17SD2f7tUhV1cRxEox3WqqUwJH2I0PRLefDOYp3SsIQOg2+E4SPUGiJnF2bK0"));
+        private static readonly string issuer = "https://localhost:7117";
+        private static readonly string audience = "SignalRBlazorChatApp";
+
+        public static string GetJwtToken()
+        {
+            SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claimsForToken = new()
+            {
+                new("userId", "e08b0077-3c15-477e-84bb-bf9d41196455")
+            };
+
+            JwtSecurityToken jwtSecurityToken = new(
+                issuer,
+                audience,
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(15),
+                signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        }
+    }
+}
---
+++ JWTGenerator/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace JWTGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMenu();
+            Console.ReadLine();
+        }
+
+        private static void PrintMenu()
+        {
+            int userSelection = 1;
+
+            while (userSelection != 0)
+            {
+                Menu();
+                userSelection = UserSelection();
+
+                if (userSelection == -1)
+                {
+                    Console.WriteLine("Invalid selection. Please try again.");
+                }
+                if (userSelection == 1)
+                {
+                    string newJwt = JwtProcessor.GetJwtToken();
+                    Console.WriteLine(newJwt);
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("\nEnter 0 to exit the program");
+            Console.WriteLine("Enter 1 to receive a JWT good for 15 minutes.\n");
+        }
+
+        private static int UserSelection()
+        {
+            try
+            {
+                Console.Write("Please make a selection: ");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
