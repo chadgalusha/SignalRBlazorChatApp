6484b9d84bdada13e6d3b90e40e55ecd535de10c
chadgalusha <chadgalusha83@gmail.com>
2023-08-09 11:33:53 -0500

    Create branch for front end development.

---
+++ SignalRBlazorChatApp/Pages/PrivateChatGroups.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRBlazorChatApp.Pages
+{
+    public class PrivateChatGroupsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
