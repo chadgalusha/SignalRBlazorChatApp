9cd04f5c0c211246d8d12a023f95c470994b4113
chadgalusha <chadgalusha83@gmail.com>
2023-08-23 13:22:34 -0500

    first commit. add DTO models to class library

---
+++ ChatApplicationModels/Dtos/ModifyPublicChatGroupDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApplicationModels.Dtos
+{
+    public class ModifyPublicChatGroupDto
+    {
+        [Key]
+        [Required]
+        public int ChatGroupId { get; set; }
+        public string ChatGroupName { get; set; } = string.Empty;
+    }
+}
---
+++ ChatApplicationModels/Dtos/ModifyPublicGroupMessageDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApplicationModels.Dtos
+{
+    public class ModifyPublicGroupMessageDto
+    {
+        [Key]
+        [Required]
+        public Guid PublicMessageId { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+        public Guid? ReplyMessageId { get; set; }
+
+        public string PictureLink { get; set; } = string.Empty;
+    }
+}
